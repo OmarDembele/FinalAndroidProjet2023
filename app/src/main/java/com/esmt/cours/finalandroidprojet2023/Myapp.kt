a7f2f10765e644e0df79df7bf0e60bd2ce49f64e
mouhssine adjassa radji <radjimouhssine@yahoo.fr>
2023-05-02 11:03:08 +0000

    Configuration du HiltModel (Ngone Diaw )

---
+++ app/src/main/java/com/esmt/cours/finalandroidprojet2023/Myapp.kt
@@ -0,0 +1,20 @@
+package com.esmt.cours.finalandroidprojet2023
+
+import android.app.Application
+import android.content.Context
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MyApp: Application() {
+    init {
+        instance = this
+    }
+
+    companion object {
+        private var instance: MyApp? = null
+
+        val context: Context
+            get() = instance!!.applicationContext
+    }
+
+}
